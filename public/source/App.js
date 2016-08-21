995157fb1f00712ecab08874ba03de568c7afc5c
Andrew Boyle <ajboyle31@gmail.com>
2016-08-21 23:36:52 +0000

    update recipes with ids

--- public/source/App.js
+++ public/source/App.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Recipes from './Recipes';
+import AddRecipe from './AddRecipe';
+
+var myRecipes = [
+  { 
+    "name": "Banana Bread",
+    "ingredients": ["Bananas", "Flour", "Eggs", "Sugar", "Love"],
+    "id": 1
+  },
+  {
+    "name": "Sugar Cookies",
+    "ingredients": ["Sugar", "Flour", "Eggs", "Love"],
+    "id": 2
+  },
+  {
+    "name": "Chocolate Brownies",
+    "ingredients": ["Chocolate", "Sugar", "Other stuff"],
+    "id": 3
+  }
+];
 
 var App = React.createClass({
-    render: function(){
-        return <h1>TESTING</h1>;
-    }
+  setInitialState: function(){
+    return {
+      recipes: []
+    };
+  },
+  componentWillMount: function(){
+    this.setState({recipes: myRecipes});
+  },
+  addRecipe: function(recipe){
+    this.state.recipe[this.state.recipe.length] = recipe;
+    this.setState({ recipes: this.state.recipe});
+  },
+  render: function(){
+    return (
+    <div>
+        <h1 id="title">My Recipe Box</h1>
+        <div id="recipeContainer">
+          <Recipes  key={this.state.recipes.length} recipes={this.state.recipes} />
+          
+          <AddRecipe />  
+        </div>
+      </div>
+    );
+  }
 });
 
 
