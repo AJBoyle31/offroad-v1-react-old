a42862571daa635921eae516f405f4bd074089f7
Andrew Boyle <ajboyle31@gmail.com>
2016-08-21 20:08:31 +0000

    first commit

---
+++ public/source/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var App = React.createClass({
+    render: function(){
+        return <h1>TESTING</h1>;
+    }
+});
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
