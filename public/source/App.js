fba8a357645a6be5582576fb7e5746ee187d3818
Andrew Boyle <ajboyle31@gmail.com>
2016-09-15 13:58:59 +0000

    removed recipeNum, added localstorage for name

--- public/source/App.js
+++ public/source/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Recipes from './Recipes';
 import AddRecipe from './AddRecipe';
+import Title from './Title.js';
 
 var myRecipes = [
   { 
@@ -21,6 +22,8 @@ var myRecipes = [
   }
 ];
 
+var name = "My";
+
 function isLocalStorageSupported(){
   try {
     localStorage.setItem("test", "test");
@@ -32,32 +35,37 @@ function isLocalStorageSupported(){
   }
 }
 
-var recipeLength = 3;
+
 
 var App = React.createClass({
   setInitialState: function(){
     return {
-      recipes: []
+      recipes: [],
+      name: ""
     };
   },
   componentWillMount: function(){
     if (isLocalStorageSupported){
       if(localStorage["recipes"] === undefined){
         localStorage.setItem("recipes", JSON.stringify(myRecipes));
+        localStorage.setItem("name", "My");
       }
       else {
         var retrievedData = localStorage.getItem("recipes");
+        var retrieveName = localStorage.getItem("name");
         myRecipes = JSON.parse(retrievedData);
+        name = JSON.parse(retrieveName);
       }
     }    
-  this.setState({recipes: myRecipes});
+    this.setState({ recipes: myRecipes,
+                    name: name  
+    });
   },
-  //need to figure this out
   addRecipe: function(recipe){
     let newRecipes = this.state.recipes.concat(recipe);
     this.setState({ recipes: newRecipes});
     localStorage.setItem("recipes", JSON.stringify(newRecipes));
-    recipeLength++;
+    
   },
   editRecipe: function(recipe, id){
     let recipeIndex = this.state.recipes.findIndex((recipe)=>recipe.id == id);
@@ -73,15 +81,19 @@ var App = React.createClass({
     this.setState({recipes: prevRecipeState});
     localStorage.setItem("recipes", JSON.stringify(prevRecipeState));
   },
+  handleNameChange: function(name){
+    this.setState({name: name});
+    localStorage.setItem("name", JSON.stringify(name));
+  },
   render: function(){
     
     return (
     <div>
-        <h1 id="title">My Recipe Box</h1>
+        <Title nameCallback={this.handleNameChange} name={this.state.name}/>
         <div id="recipeContainer">
-          <Recipes  key={recipeLength} recipes={this.state.recipes} editRecipe={this.editRecipe} deleteRecipe={this.deleteRecipe} />
+          <Recipes   recipes={this.state.recipes} editRecipe={this.editRecipe} deleteRecipe={this.deleteRecipe} />
           
-          <AddRecipe addRecipe={this.addRecipe} />  
+          <AddRecipe addRecipe={this.addRecipe} recipes={this.state.recipes}/>  
         </div>
       </div>
     );
