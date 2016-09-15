4f7f98d263346f6dd8a81c5686990ced9005b4f1
Andrew Boyle <ajboyle31@gmail.com>
2016-09-15 13:58:21 +0000

    added to allow user to change titel

---
+++ public/source/Title.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Button, Modal, FormGroup, Form, Col, FormControl} from 'react-bootstrap';
+
+
+var Title = React.createClass({
+    getInitialState: function(){
+      return {showTitle: false};  
+    },
+    open: function(){
+        this.setState({showTitle: true});
+    },
+    close: function(){
+        this.setState({showTitle: false});
+    },
+    render: function(){
+        return (
+            <div>
+            <h1 id="title"><a onClick={this.open}>{this.props.name} Recipe Box</a></h1>
+            <Modal show={this.state.showTitle} onHide={this.close}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Recipe Box Title</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form horizontal>
+                        <FormGroup controlId="formHorizontalTitleName">
+                            <Col sm={2}>
+                                Title:
+                            </Col>
+                            <Col sm={10}>
+                                <FormControl type="text" defaultValue={this.props.name}/>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={this.handleTitleChange}>Confirm</Button>
+                    <Button onClick={this.close}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+            </div>
+        );
+    }
+});
+
+export default Title;
