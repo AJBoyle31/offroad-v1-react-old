0658ccceed7e268642f3e8bd2afcf05ab95b680e
Andrew Boyle <ajboyle31@gmail.com>
2016-09-17 14:51:02 +0000

    commented out localstorage, added directions

--- public/source/App.js
+++ public/source/App.js
@@ -8,16 +8,19 @@ var myRecipes = [
   { 
     "name": "Banana Bread",
     "ingredients": ["Bananas", "Flour", "Eggs", "Sugar", "Love"],
+    "directions": ["Set oven to 350 degrees", "Combine all ingredients", "Bake for 30 minutes"],
     "id": 1
   },
   {
     "name": "Sugar Cookies",
     "ingredients": ["Sugar", "Flour", "Eggs", "Love"],
+    "directions": ["Set oven to 400 degrees", "Combine ingredients", "Bake for 10 minutes", "Serve hot"],
     "id": 2
   },
   {
     "name": "Chocolate Brownies",
     "ingredients": ["Chocolate", "Sugar", "Other stuff"],
+    "directions": ["Set oven to 450 degrees", "Combine ingredients", "Pour batter in pan", "Bake for 45 minutes", "Let cool for 10 minutes"],
     "id": 3
   }
 ];
@@ -45,6 +48,9 @@ var App = React.createClass({
     };
   },
   componentWillMount: function(){
+    
+    /*  Commented out all local stoarge in order to work on directions
+    
     if (isLocalStorageSupported){
       if(localStorage["recipes"] === undefined){
         localStorage.setItem("recipes", JSON.stringify(myRecipes));
@@ -60,7 +66,7 @@ var App = React.createClass({
           name = JSON.parse(retrieveName);
         }
       }
-    }    
+    }    */
     this.setState({ recipes: myRecipes,
                     name: name  
     });
@@ -68,7 +74,7 @@ var App = React.createClass({
   addRecipe: function(recipe){
     let newRecipes = this.state.recipes.concat(recipe);
     this.setState({ recipes: newRecipes});
-    localStorage.setItem("recipes", JSON.stringify(newRecipes));
+    //localStorage.setItem("recipes", JSON.stringify(newRecipes));
     
   },
   editRecipe: function(recipe, id){
@@ -76,18 +82,18 @@ var App = React.createClass({
     let oldRecipes = this.state.recipes;
     oldRecipes[recipeIndex] = recipe;
     this.setState({ recipes: oldRecipes});
-    localStorage.setItem("recipes", JSON.stringify(oldRecipes));
+    //localStorage.setItem("recipes", JSON.stringify(oldRecipes));
   },
   deleteRecipe: function(id){
     let recipeIndex = this.state.recipes.findIndex((recipe)=>recipe.id == id);
     let prevRecipeState = this.state.recipes;
     prevRecipeState.splice(recipeIndex, 1);
     this.setState({recipes: prevRecipeState});
-    localStorage.setItem("recipes", JSON.stringify(prevRecipeState));
+    //localStorage.setItem("recipes", JSON.stringify(prevRecipeState));
   },
   handleNameChange: function(name){
     this.setState({name: name});
-    localStorage.setItem("name", JSON.stringify(name));
+    //localStorage.setItem("name", JSON.stringify(name));
   },
   render: function(){
     
