feac29f757734fb75fedc4d7b29548f5e7dcf0a4
Andrew Boyle <ajboyle31@gmail.com>
2016-09-17 14:50:03 +0000

    added directions prop

--- public/source/Recipes.js
+++ public/source/Recipes.js
@@ -11,7 +11,8 @@ var Recipes = React.createClass({
                 <Recipe key={recipe.id} 
                   id={recipe.id} 
                   name={recipe.name} 
-                  ingredients={recipe.ingredients} 
+                  ingredients={recipe.ingredients}
+                  directions={recipe.directions}
                   editRecipe={this.props.editRecipe} 
                   deleteRecipe={this.props.deleteRecipe} /> 
                 
